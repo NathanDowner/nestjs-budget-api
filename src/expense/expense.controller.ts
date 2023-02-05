4e58358ecefd69663c892c5700bf02604e50c35c
Nathan Downer <nathandowner123@gmail.com>
2023-02-05 17:29:34 -0500

    secure expense endpoints

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -6,21 +6,34 @@ import {
   Delete,
   ParseIntPipe,
   Put,
+  Post,
 } from '@nestjs/common';
 import { ExpenseService } from './expense.service';
 import { UpdateExpenseDto } from './dto/update-expense.dto';
-import { ApiNoContentResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ExpenseDto } from './dto/expense.dto';
 import { PurchaseDto } from 'src/purchase/dto/purchase.dto';
+import { CreatePurchaseDto } from 'src/purchase/dto/create-purchase.dto';
+import { PurchaseService } from 'src/purchase/purchase.service';
 
 @ApiTags('expenses')
 @Controller('expenses')
 export class ExpenseController {
-  constructor(private readonly expenseService: ExpenseService) {}
+  constructor(
+    private readonly expenseService: ExpenseService,
+    private readonly purchaseService: PurchaseService,
+  ) {}
 
   @Get(':id')
   @ApiOkResponse({ type: ExpenseDto })
+  @ApiBearerAuth()
   @Serialize(ExpenseDto)
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.expenseService.findOne(id);
@@ -28,6 +41,7 @@ export class ExpenseController {
 
   @Put(':id')
   @ApiOkResponse({ type: ExpenseDto })
+  @ApiBearerAuth()
   @Serialize(ExpenseDto)
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -38,6 +52,7 @@ export class ExpenseController {
 
   @Get(':id/purchases')
   @ApiOkResponse({ type: [ExpenseDto] })
+  @ApiBearerAuth()
   @Serialize(PurchaseDto)
   findPurchases(@Param('id', ParseIntPipe) id: number) {
     return this.expenseService.findPurchases(id);
@@ -45,8 +60,22 @@ export class ExpenseController {
 
   @Delete(':id')
   @ApiNoContentResponse()
+  @ApiBearerAuth()
   remove(@Param('id', ParseIntPipe) id: number) {
     this.expenseService.remove(id);
     return;
+  }
+
+  @Post(':id/purchases')
+  @ApiTags('purchases')
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: PurchaseDto })
+  @Serialize(PurchaseDto)
+  async createPurchase(
+    @Param('id', ParseIntPipe) expenseId: number,
+    @Body() createPurchaseDto: CreatePurchaseDto,
+  ) {
+    const expense = await this.expenseService.findOne(expenseId);
+    return this.purchaseService.create(createPurchaseDto, expense);
   }
 }
